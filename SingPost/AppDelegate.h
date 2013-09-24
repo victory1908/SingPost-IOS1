417a0c4c69cfc00b951002d37da7344287a7e404
Edward Soetiono <sedward@live.com.sg>
2013-09-24 14:05:38 +0800

    Initial Commit

---
+++ SingPost/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SingPost
+//
+//  Created by Edward Soetiono on 24/9/13.
+//  Copyright (c) 2013 Codigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SingPost/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SingPost
+//
+//  Created by Edward Soetiono on 24/9/13.
+//  Copyright (c) 2013 Codigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
