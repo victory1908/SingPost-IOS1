91fa0e3b905c2d4f3deb7ef23b18fb0e24155057
Edward Soetiono <sedward@live.com.sg>
2013-10-01 16:20:06 +0800

    added scroller in locate us details

--- SingPost/ViewControllers/LocateUs/LocateUsDetailsViewController.m
+++ SingPost/ViewControllers/LocateUs/LocateUsDetailsViewController.m
@@ -15,10 +15,17 @@
 #import "MapAnnotation.h"
 #import "UIFont+SingPost.h"
 #import "UILabel+VerticalAlign.h"
+#import "SectionToggleButton.h"
 
 @interface LocateUsDetailsViewController () <MKMapViewDelegate>
 
 @end
+
+typedef enum  {
+    LOCATEUSDETAILS_SECTION_OPENINGHOURS,
+    LOCATEUSDETAILS_SECTION_SERVICES,
+    LOCATEUSDETAILS_SECTION_POSTINGBOX
+} tLocateUsDetailsSections;
 
 @implementation LocateUsDetailsViewController
 {
@@ -26,6 +33,7 @@
     MKMapView *locationMapView;
     UILabel *addressLabel;
     UIImageView *isOpenedIndicatorImageView;
+    SectionToggleButton *openingHoursSectionButton, *servicesSectionButton, *postingBoxSectionButton;
 }
 
 //designated initializer
@@ -77,6 +85,38 @@
     [searchMapButton setFrame:CGRectMake(270, 256, 44, 44)];
     [contentView addSubview:searchMapButton];
     
+    UIView *topSeparatorView = [[UIView alloc] initWithFrame:CGRectMake(0, 310, contentView.bounds.size.width, 1)];
+    [topSeparatorView setBackgroundColor:RGB(196, 197, 200)];
+    [contentView addSubview:topSeparatorView];
+    
+    UIView *bottomSeparatorView = [[UIView alloc] initWithFrame:CGRectMake(0, 361, contentView.bounds.size.width, 1)];
+    [bottomSeparatorView setBackgroundColor:RGB(196, 197, 200)];
+    [contentView addSubview:bottomSeparatorView];
+    
+    openingHoursSectionButton = [[SectionToggleButton alloc] initWithFrame:CGRectMake(0, 311, 107, 50)];
+    [openingHoursSectionButton setTag:LOCATEUSDETAILS_SECTION_OPENINGHOURS];
+    [openingHoursSectionButton.titleLabel setFont:[UIFont SingPostBoldFontOfSize:12.0f fontKey:kSingPostFontOpenSans]];
+    [openingHoursSectionButton addTarget:self action:@selector(sectionButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [openingHoursSectionButton setTitle:@"Opening hours" forState:UIControlStateNormal];
+    [openingHoursSectionButton setSelected:YES];
+    [contentView addSubview:openingHoursSectionButton];
+    
+    servicesSectionButton = [[SectionToggleButton alloc] initWithFrame:CGRectMake(107, 311, 107, 50)];
+    [servicesSectionButton setTag:LOCATEUSDETAILS_SECTION_SERVICES];
+    [servicesSectionButton.titleLabel setFont:[UIFont SingPostBoldFontOfSize:12.0f fontKey:kSingPostFontOpenSans]];
+    [servicesSectionButton addTarget:self action:@selector(sectionButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [servicesSectionButton setTitle:@"Services" forState:UIControlStateNormal];
+    [servicesSectionButton setSelected:NO];
+    [contentView addSubview:servicesSectionButton];
+    
+    postingBoxSectionButton = [[SectionToggleButton alloc] initWithFrame:CGRectMake(214, 311, 108, 50)];
+    [postingBoxSectionButton setTag:LOCATEUSDETAILS_SECTION_POSTINGBOX];
+    [postingBoxSectionButton.titleLabel setFont:[UIFont SingPostBoldFontOfSize:12.0f fontKey:kSingPostFontOpenSans]];
+    [postingBoxSectionButton addTarget:self action:@selector(sectionButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [postingBoxSectionButton setTitle:@"Posting Box" forState:UIControlStateNormal];
+    [postingBoxSectionButton setSelected:NO];
+    [contentView addSubview:postingBoxSectionButton];
+    
     self.view = contentView;
 }
 
@@ -87,7 +127,7 @@
     //map
     MKCoordinateRegion mapRegion;
     mapRegion.center = CLLocationCoordinate2DMake(_entityLocation.latitude.floatValue, _entityLocation.longitude.floatValue);
-    mapRegion.span = MKCoordinateSpanMake(0.015, 0.015);
+    mapRegion.span = MKCoordinateSpanMake(0.016, 0.016);
     [locationMapView setRegion:mapRegion animated:YES];
     
     MapAnnotation *locationAnnotation = [[MapAnnotation alloc] initWithLatitude:_entityLocation.latitude.floatValue andLongitude:_entityLocation.longitude.floatValue];
@@ -95,10 +135,18 @@
     
     //fields
     [addressLabel setText:_entityLocation.address];
-//    [addressLabel sizeThatFits:addressLabel.bounds.size];
-//    [addressLabel setCenter:CGPointMake(addressLabel.center.x, 276)];
     [isOpenedIndicatorImageView setImage:[UIImage imageNamed:_entityLocation.isOpened ? @"green_circle" : @"red_circle"]];
 }
+
+#pragma mark - IBActions
+
+- (IBAction)sectionButtonClicked:(id)sender
+{
+    [openingHoursSectionButton setSelected:(openingHoursSectionButton == sender)];
+    [servicesSectionButton setSelected:(servicesSectionButton == sender)];
+    [postingBoxSectionButton setSelected:(postingBoxSectionButton == sender)];
+}
+
 
 #pragma mark - MKMapViewDelegates
 
