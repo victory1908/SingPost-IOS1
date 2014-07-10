ba758290b9dc4ae59ae7509268075b01629ecbbd
Wei Guang <weiguang@icloud.com>
2014-07-10 17:13:01 +0800

    Carry on refreshing location

--- SingPost/ViewControllers/LocateUs/LocateUsListViewController.m
+++ SingPost/ViewControllers/LocateUs/LocateUsListViewController.m
@@ -324,7 +324,7 @@
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
 {
-    [self discardLocationManager];
+    //[self discardLocationManager];
     _cachedUserLocation = newLocation;
     [locationsTableView reloadData];
 }
