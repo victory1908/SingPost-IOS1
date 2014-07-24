0983a9c6567e5f4d61f038477cfbe3d8ba38c000
Wei Guang <weiguang@icloud.com>
2014-07-24 17:12:21 +0800

    warning message

--- SingPost/AppDelegate.m
+++ SingPost/AppDelegate.m
@@ -63,6 +63,7 @@
     [[GAI sharedInstance] setTrackUncaughtExceptions:YES];
     [GAI sharedInstance].dispatchInterval = 15;
     [[GAI sharedInstance] trackerWithTrackingId:GAI_ID];
+#warning MAKE SURE UNCOMMENT WHEN SUBMITTING!
     //[[GAI sharedInstance] trackerWithTrackingId:GAI_SINGPOST_ID];
 }
 
