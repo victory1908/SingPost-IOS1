8c10d4df7ea77f8f9aef10f4903c2725e531f625
Wei Guang <weiguang@icloud.com>
2015-07-09 11:44:09 +0800

    Clean up

--- SingPost/Constants.h
+++ SingPost/Constants.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Codigo. All rights reserved.
 //
 
+#define FACEBOOK_UAT_KEY @"1459873204283423"
+
 #define SINGPOST_ITUNES_STORE_URL @"https://itunes.apple.com/sg/app/singpost-mobile/id647986630"
 
 #define SIDEBAR_WIDTH 275.0f
@@ -22,5 +24,14 @@
 //Codigo
 #define GAI_ID @"UA-46197062-1"
 
-// ipad frame
-#define INTERFACE_IS_IPAD ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+//Error messages
+#define INCOMPLETE_FIELDS_ERROR             @"Please ensure that all fields are entered correctly."
+#define NO_INTERNET_ERROR_TITLE             @"Something has gone wrong!"
+#define NO_INTERNET_ERROR                   @"Connection to the server failed. Please try again later."
+#define NO_RESULTS_ERROR                    @"Sorry, there are no results found. Please try again."
+#define NO_TRACKING_NUMBER_ERROR            @"Please enter tracking number."
+#define INVALID_TRACKING_NUMBER_ERROR       @"Please enter a valid tracking number."
+#define TRACKED_ITEM_NOT_FOUND_ERROR        @"Item number incorrect or tracking status for this item is not yet updated. Please try again later."
+#define NEW_TRACKED_ITEM_NOT_FOUND_ERROR    @"Item number incorrect or tracking status for this item is not yet updated. Please try again later. This number will be stored for your easy reference later."
+#define ERRORCODE1001_MESSAGE               @"We are now encountering very high traffic in our system, please try again later."
+#define ITEM_NUMBER_NOT_FOUND               @"Item number not found. Please check the number."
